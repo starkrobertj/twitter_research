e472899307150571bfadb93f4aa17c39333dea1c
Robert Stark <stark.robert.j@gmail.com>
2012-06-29 07:54:05 -0600

    Initial import after cleaning things up

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,10 @@
+class SearchesController < ApplicationController
+
+  def index
+  end
+
+  def show
+    search = SimpleTwitter::Search.new
+    @results = search.search(params[:q])
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterResearch::Application.config.secret_token = '578b5d2169f98a312d0f998226f1422e583dcd38dfe3fb14f782caa230891787ed42ddf183a6d93d00d2540250d0ead2804ee1633b8aeb36875948ceb19da995'
